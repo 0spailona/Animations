d13738db965a7d55891397c07dacb968adb8ec36
Daria <n-d-p@mail.ru>
2024-03-13 18:15:06 +0300

    it works

--- README.md
+++
@@ -1,1 +0,0 @@
-##CI [![Build status](https://ci.appveyor.com/api/projects/status/fenmgc1ne5gsky3m?svg=true)](https://ci.appveyor.com/project/0spailona/animations)
--- src/index.js
+++
@@ -1,5 +0,0 @@
-import './style.css'
-import Collapse from "./Collapse";
-
-const collapse = new Collapse(document.querySelector('.collapsibleBtn'),document.querySelector('.fable'));
-collapse.addListener()
